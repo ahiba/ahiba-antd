a076e7003e56a01ce191a3b1cff2b4bf4f509f07
詹周东 <zhanzhoudong@adpanshi.com>
2020-03-22 21:32:44 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import './styles/index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={e => {
+        console.log(134)
+      }}>hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
     </div>
   );
 }
