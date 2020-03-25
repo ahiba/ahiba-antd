200bd6533fff6a48e11b0b064823d6496979b91a
zhanzhoudong <zhanzhoudong@adpanshi.com>
2020-03-25 13:37:27 +0800

    'transition'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons'
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
 import SubMenu from './components/Menu/subMenu'
-import './styles/index.scss';
+import Icon from './components/Icon/icon'
+import Transition from './components/Transition/transition'
+
+
+import './styles/index.scss'
+
+library.add(fas)
 
 function App() {
+  const [ show, setShow ] = useState(false)
   return (
     <div className="App">
-      <Menu defaultIndex={'0'} onSelect={(index) => {alert(index)}} mode="vertical"
+      <Button size={ButtonSize.Large} onClick={() => {setShow(!show)}}>Toggle</Button>
+      
+      <Transition
+        in={show}
+        timeout={300}
+        animation="zoom-in-left"
+      >
+        <div>
+          <p>edit <code>src/App.tsx</code> and save to reload</p>
+          <p>edit <code>src/App.tsx</code> and save to reload</p>
+          <p>edit <code>src/App.tsx</code> and save to reload</p>
+          <p>edit <code>src/App.tsx</code> and save to reload</p>
+          <p>edit <code>src/App.tsx</code> and save to reload</p>
+        </div>
+      </Transition>
+      <Icon icon="coffee" theme="danger" size="10x"  />
+      <Menu 
+        defaultIndex={'0'} 
+        onSelect={(index) => {alert(index)}} 
         defaultOpenSubMenus={['2']}
       >
         <MenuItem>
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,7 +1,11 @@
 import React, { useContext, FunctionComponentElement, useState } from 'react'
 import classNames from 'classnames'
+import { CSSTransition } from 'react-transition-group'
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem'
+import Icon from '../Icon/icon';
+import Transition from '../Transition/transition'
 
 export interface SubMenuProps {
   index?: string ;
@@ -16,7 +20,9 @@ const SubMenu: React.FC<SubMenuProps> = ({index, title, children, className}) =>
   const [ menuOpen, setOpen ] = useState(isOpened)
 
   const classes = classNames('menu-item submenu-item', className, {
-    'is-active': context.index === index
+    'is-active': context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode === 'vertical'
   })
   const handleClick = (e: React.MouseEvent) => {
     console.log('点击了')
@@ -57,15 +63,22 @@ const SubMenu: React.FC<SubMenuProps> = ({index, title, children, className}) =>
       }
     })
     return (
-      <ul className={subMenuClasses}>
-        {childrenComponent}
-      </ul>
+      <Transition
+        in={menuOpen}
+        timeout={300}
+        animation="zoom-in-top"
+      >
+        <ul className={subMenuClasses}>
+          {childrenComponent}
+        </ul>
+      </Transition>
     )
   }
   return (
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
